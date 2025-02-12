623345f7c02054d10784b94bafb7d2f4bd9fd4bf
Abhishek Rawat <126187746+abhirawat24@users.noreply.github.com>
2025-02-13 00:45:33 +0530

    Update variables.js

--- variables.js
+++ variables.js
@@ -2,6 +2,7 @@ const accountId = 263126;
 let accountEmail = "ababab@xys";
 var accountVar = "qwerty123";
 accountCity = "Almora";
+let accountState;
 
 accountEmail = "qwer@gamil.com";
 accountVar = "12345";
@@ -9,3 +10,8 @@ accountCity = "Bageshwar";
 
 console.log(accountId);
 console.table([accountEmail, accountVar, accountCity]);
+
+/*
+The constant won't change, while the let, var and the one with no type defined get changed when the code is run on the terminal,
+the variable in which the state is not defined come as undefined.
+*/
