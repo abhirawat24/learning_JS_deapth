73b65bc2a8567ff4de13c949f53347e19927b2ce
Abhishek Rawat <126187746+abhirawat24@users.noreply.github.com>
2025-02-13 01:15:41 +0530

    Create dataTypes.js

---
+++ dataTypes.js
@@ -0,0 +1,30 @@
+"use strict";//using this treats the JS code as newer version
+
+//alert(2+2);// This wont work in node.js but will work in the browser
+
+console.log
+( 3+
+       3
+);//code redability should be high
+
+console.log("Abhishek");
+let name = "Abhishek";
+let age = "20";
+let status = false;
+
+
+//number => 2 powr 53
+//bigint
+//string =>""
+//boolean => true/false
+//null => standalone value
+//undefined
+//symbol
+
+//objects
+
+console.log(typeof "Abhishek");
+
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
