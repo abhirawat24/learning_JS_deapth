7d6772e6da5df61e7a646a5833d8e1365ba10ea1
Abhishek Rawat <126187746+abhirawat24@users.noreply.github.com>
2025-02-13 00:39:37 +0530

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,11 @@
+const accountId = 263126;
+let accountEmail = "ababab@xys";
+var accountVar = "qwerty123";
+accountCity = "Almora";
+
+accountEmail = "qwer@gamil.com";
+accountVar = "12345";
+accountCity = "Bageshwar";
+
+console.log(accountId);
+console.table([accountEmail, accountVar, accountCity]);
