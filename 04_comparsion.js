16d0e9b9e39df367daf8d88ccaf18f60fec60014
Abhishek Rawat <126187746+abhirawat24@users.noreply.github.com>
2025-02-14 01:34:02 +0530

    Create 04_comparsion.js

---
+++ 04_comparsion.js
@@ -0,0 +1,21 @@
+console.log(9 > 8);
+console.log(9 < 8);
+console.log(9 >= 8);
+console.log(9 != 8);
+console.log(8 == 9);
+
+console.log("2" > 1);
+console.log(2 > "1");
+
+cconsole.log(null > 8);
+console.log(null == 8);
+console.log(null >= 8);
+
+
+console.log(undefined > 0);
+console.log(undefined < 0);
+console.log(undefined == 0);
+
+//null and undefined conversions are mostly avoided
+
+console.log("2" === 2);
